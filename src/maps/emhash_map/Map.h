29446e6097a0b27079306850fd8772c5326d011d
renzibei <31380016+renzibei@users.noreply.github.com>
2022-03-20 14:03:41 +0800

    upadte emhash_map, pass compile

--- src/maps/emhash_map/Map.h
+++ src/maps/emhash_map/Map.h
@@ -2,10 +2,13 @@
 
 #include "Hash.h"
 #include "Allocator.h"
+// define EMH_BUCKET_INDEX for the compiling for hash_table7
+#define EMH_BUCKET_INDEX 2
 #include "emhash/hash_table7.hpp"
 //#include "emhash/emilib2.hpp"
 
-static const char* MAP_NAME = "emlib2::hash_map2";
+static const char* MAP_NAME = "emhash::hash_map7";
+//static const char* MAP_NAME = "emlib::hash_map2";
 
 template <class Key, class T>
 using Map = emhash7::HashMap<Key, T, Hash<Key>, std::equal_to<>>;
